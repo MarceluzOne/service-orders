594dee88f3f0f0dbfcf034162c63d689b0e2f87d
thialylima <thialymedeiros@gmail.com>
2025-01-30 21:36:26 -0300

    POP UP COM ORDEM DE SERVIÇO , NÃO MANDAR PRA MASTER AINDA!!!

--- src/app/services/equipment/equipment.service.ts
+++ src/app/services/equipment/equipment.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
@@ -11,13 +11,24 @@ export class EquipmentService {
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
+  // Função para obter a lista de equipamentos
   public getEquipament(): Observable<any> {
     const headers = this.authService.getAuthHeaders(); 
     return this.http.get(`${environment.apiUrl}/api/equipments/all`, { headers });
   }
 
+  // Função para registrar um novo equipamento
   public registerEquipament(data: any): Observable<any> {
     const headers = this.authService.getAuthHeaders(); 
     return this.http.post(`${environment.apiUrl}/api/equipments`, data, { headers });
   }
+
+  // Função para obter todas as ordens de serviço
+  public getAllServiceOrders(): Observable<any> {
+    const headers = this.authService.getAuthHeaders();
+    return this.http.get(`${environment.apiUrl}/api/service-orders/all`, { headers });
+  }
+
+  
+
 }
